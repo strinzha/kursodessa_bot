e7e448af912a0f06421a2b47f359f3c8b0384362
Boris Strinzha <strinzha@Boriss-MacBook-Pro.local>
2016-06-12 16:31:18 +0300

    new bot

--- app.js
+++ app.js
@@ -1,50 +1,30 @@
 var TelegramBot = require('node-telegram-bot-api');
 var token = '214439719:AAEPy705OqV9XeA1slacsEBLsQHWz2jvu08';
 var bot = new TelegramBot(token, {polling: true});
+var axios = require('axios');
 
-
-var kurs  = {
-  '7 км': {
-    'USD': 'Выходной',
-    'EUR': 'Выходной',
-    'RUR': 'Выходной'
-  },
-  'Книжка': {
-    'USD': '25.00 / 25.30',
-    'EUR': '28.10 / 28.70',
-    'RUR': '0.370 / 0.400'
-  },
-  'Заболотного': {
-    'USD': '25.00 / 25.30',
-    'EUR': '28.10 / 28.70',
-    'RUR': '0.370 / 0.400'
-  },
-  'Бочарова': {
-    'USD': '25.00 / 25.30',
-    'EUR': '28.10 / 28.70',
-    'RUR': '0.370 / 0.400'
-  }
-}
 
 var today = new Date();
 var dd = today.getDate();
 var mm = ("0" + (today.getMonth()+1)).slice(-2); //January is 0!
 var yyyy = today.getFullYear();
-
-var message = 'Курс в Одессе на ' + dd + '.' + mm + '.' + yyyy + '\n\n';
-
-for (var i in kurs ) {
-  if (kurs.hasOwnProperty(i)) {
-    message += i + ':\n' + 'USD: ' + kurs[i].USD + '\n' + 'EUR: ' + kurs[i].EUR + '\n' + 'RUR: ' + kurs[i].RUR + '\n\n';
-  }
-}
+var message = '';
 
 bot.onText(/\/курс/, function (msg, match) {
   var userId = msg.from.id;
-  bot.sendMessage(userId, message);
-});
+  axios.get('http://bot.strinzha.com/api.php')
+    .then(function(response) {
 
-bot.onText(/\/тютакатышотам/, function (msg, match) {
-  var userId = msg.from.id;
-  bot.sendMessage(userId, 'та ниче бобра даю');
+      console.log(response.data);
+      message = 'Курс в Одессе на ' + dd + '.' + mm + '.' + yyyy + '\n\n';
+
+      var kurs = response.data;
+      for (var i in kurs ) {
+        if (kurs.hasOwnProperty(i)) {
+          message += i + ':\n' + 'USD: ' + kurs[i].USD + '\n' + 'EUR: ' + kurs[i].EUR + '\n' + 'RUR: ' + kurs[i].RUR + '\n\n';
+        }
+      }
+
+      bot.sendMessage(userId, message);
+    });
 });
